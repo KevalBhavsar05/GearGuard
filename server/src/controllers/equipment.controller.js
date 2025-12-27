635902d21d23dc31f794aa5b000bbf5c5f1ef55b
KevalBhavsar05 <18kevalbhavsar@gmail.com>
2025-12-27 13:14:40 +0530

    geteqp api

--- server/src/controllers/equipment.controller.js
+++ server/src/controllers/equipment.controller.js
@@ -1,11 +1,25 @@
-import maintainancerequest from "../models/maintainancerequest.model.js";
+import Department from "../models/department.model.js";
 import equipmentModel from "../models/equipment.model.js";
+
 const addEquipment = async (req, res) => {};
+
 export const getAllEquipmentByDeptComp = async (req, res) => {
   try {
     const user = req.user;
-    console.log();
-    
+    const equipments = await equipmentModel
+      .find({
+        department_id: user.departmentId,
+        companyId: user.companyId,
+      })
+      .populate("department_id")
+      .populate("assigned_to", "-password");
+    if (equipments.length === 0) {
+      return res.status(404).json({
+        message: "No equipment found for this department and company.",
+      });
+    }
+
+    return res.status(200).json({ equipments });
   } catch (error) {
     return res
       .status(500)
